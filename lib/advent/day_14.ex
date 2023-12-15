93280986d5ead98fcc52dff959935f8fe3a74fa9
c-brenn <conor@sketch.com>
2023-12-15 09:34:46 +0000

    Day 14 part 2

--- lib/advent/day_14.ex
+++ lib/advent/day_14.ex
@@ -12,11 +12,56 @@ defmodule Advent.Day14 do
   def part_one(text) do
     text
     |> Parser.columns()
-    |> tilt_north()
+    |> tilt()
     |> count_load()
   end
 
-  def tilt_north(columns) do
+  @cycles 1_000_000_000
+
+  def part_two(text) do
+    {cycle_start, cycle_end, state_cache} =
+      text
+      |> Parser.columns()
+      |> find_cycle()
+
+    cycle_period = cycle_end - cycle_start
+
+    offset = rem(@cycles - cycle_end, cycle_period)
+    end_index = cycle_start + offset
+    {state, _} = Enum.find(state_cache, fn {_k, v} -> v == end_index end)
+
+    count_load(state)
+  end
+
+  def rotate(grid, :north_to_west) do
+    grid
+    |> Enum.reverse()
+    |> Enum.zip()
+    |> Enum.map(&(&1 |> Tuple.to_list() |> Enum.reverse()))
+  end
+
+  def rotate(grid, :west_to_south) do
+    grid
+    |> Enum.reverse()
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+    |> Enum.reverse()
+  end
+
+  def rotate(grid, :south_to_east) do
+    grid
+    |> Enum.zip()
+    |> Enum.map(&Tuple.to_list/1)
+  end
+
+  def rotate(grid, :east_to_north) do
+    grid
+    |> Enum.zip()
+    |> Enum.map(&(&1 |> Tuple.to_list() |> Enum.reverse()))
+    |> Enum.reverse()
+  end
+
+  def tilt(columns) do
     columns
     |> Enum.map(fn column ->
       column
@@ -36,6 +81,15 @@ defmodule Advent.Day14 do
     end)
   end
 
+  def cycle(grid) do
+    [:north_to_west, :west_to_south, :south_to_east, :east_to_north]
+    |> Enum.reduce(grid, fn dir, acc ->
+      acc
+      |> tilt()
+      |> rotate(dir)
+    end)
+  end
+
   defp count_load([first_column | _] = sorted_columns) do
     offset = Enum.count(first_column) + 1
 
@@ -50,5 +104,20 @@ defmodule Advent.Day14 do
 
       acc + column_load
     end)
+  end
+
+  defp find_cycle(grid) do
+    find_cycle(grid, 0, %{grid => 0})
+  end
+
+  defp find_cycle(grid, cycle, cache) do
+    grid = cycle(grid)
+    cycle = cycle + 1
+
+    if index = Map.get(cache, grid) do
+      {index, cycle, cache}
+    else
+      find_cycle(grid, cycle, Map.put(cache, grid, cycle))
+    end
   end
 end
--- test/advent/day_14_test.exs
+++ test/advent/day_14_test.exs
@@ -33,7 +33,7 @@ defmodule Advent.Day14Test do
     end
   end
 
-  describe "tilt_north/1" do
+  describe "tilt/1" do
     test "example input" do
       columns = Day14.Parser.columns(@example_input)
 
@@ -52,7 +52,90 @@ defmodule Advent.Day14Test do
         """
         |> Day14.Parser.columns()
 
-      assert Day14.tilt_north(columns) == expected
+      assert Day14.tilt(columns) == expected
+    end
+  end
+
+  describe "rotate/1" do
+    test "rotates -90 degrees" do
+      north = Day14.Parser.columns(@example_input)
+
+      assert north
+             |> Day14.rotate(:north_to_west)
+             |> Day14.rotate(:west_to_south)
+             |> Day14.rotate(:south_to_east)
+             |> Day14.rotate(:east_to_north) ==
+               north
+
+      west = Day14.rotate(north, :north_to_west)
+
+      assert west == [
+               ~c"O....#....",
+               ~c"O.OO#....#",
+               ~c".....##...",
+               ~c"OO.#O....O",
+               ~c".O.....O#.",
+               ~c"O.#..O.#.#",
+               ~c"..O..#O..O",
+               ~c".......O..",
+               ~c"#....###..",
+               ~c"#OO..#...."
+             ]
+
+      south = Day14.rotate(west, :west_to_south)
+
+      assert south == [
+               ~c"...O#.O.#.",
+               ~c".....#....",
+               ~c".#O.#O....",
+               ~c".#.O...#..",
+               ~c"##.#O..#.#",
+               ~c"......O.#.",
+               ~c"......#.O.",
+               ~c"O..O#...O.",
+               ~c"O....OO...",
+               ~c"##..O.O.OO"
+             ]
+
+      east = Day14.rotate(south, :south_to_east)
+
+      assert east == [
+               ~c"....#..OO#",
+               ~c"..###....#",
+               ~c"..O.......",
+               ~c"O..O#..O..",
+               ~c"#.#.O..#.O",
+               ~c".#O.....O.",
+               ~c"O....O#.OO",
+               ~c"...##.....",
+               ~c"#....#OO.O",
+               ~c"....#....O"
+             ]
+
+      assert Day14.rotate(east, :east_to_north) == north
+    end
+  end
+
+  describe "cycle/1" do
+    test "tilts in each direction" do
+      grid = Day14.Parser.columns(@example_input)
+
+      expected =
+        """
+        .....#....
+        ....#...O#
+        ...OO##...
+        .OO#......
+        .....OOO#.
+        .O#...O#.#
+        ....O#....
+        ......OOOO
+        #...O###..
+        #..OO#....
+        """
+        |> Day14.Parser.columns()
+
+      assert Day14.cycle(grid) == expected
     end
   end
 
@@ -65,6 +148,17 @@ defmodule Advent.Day14Test do
       input = File.read!("priv/inputs/day-14.txt")
 
       assert Day14.part_one(input) == 108_955
+    end
+  end
+
+  describe "part_two/1" do
+    test "example input" do
+      assert Day14.part_two(@example_input) == 64
+    end
+
+    test "input" do
+      input = File.read!("priv/inputs/day-14.txt")
+      assert Day14.part_two(input) == 106_689
     end
   end
 end
